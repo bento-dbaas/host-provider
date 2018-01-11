d166c8e25a48c121b78e398a486271f26aebcf07
otherpirate <mauro_murari@hotmail.com>
2018-01-11 09:28:16 -0200

    Add mongodb config

--- host_provider/settings.py
+++ host_provider/settings.py
@@ -2,3 +2,10 @@ from os import getenv
 
 
 LIBCLOUD_CA_CERTS_PATH = getenv("LIBCLOUD_CA_CERTS_PATH", None)
+
+
+MONGODB_HOST = getenv("MONGODB_HOST", "127.0.0.1")
+MONGODB_PORT = getenv("MONGODB_PORT", 27017)
+MONGODB_DB = getenv("MONGODB_DB", "host_provider")
+MONGODB_USER = getenv("MONGODB_USER", None)
+MONGODB_PWD = getenv("MONGODB_PWD", None)
