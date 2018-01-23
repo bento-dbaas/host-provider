bbb1414908f1d7d5fe19902a174ad9d3cd4e9673
otherpirate <mauro_murari@hotmail.com>
2018-01-23 10:04:37 -0200

    Add route to delete credential, stop/start and destroy node

--- host_provider/main.py
+++ host_provider/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify, make_response
+from host_provider.credentials.base import CredentialAdd
 from host_provider.providers import get_provider_to
 from host_provider.models import Host
 from traceback import print_exc
@@ -22,9 +23,9 @@ def create_host(provider_name, env):
     try:
         provider_cls = get_provider_to(provider_name)
         provider = provider_cls(env, engine)
-        node = provider._create_host(cpu, memory, name, group)
-    except Exception as e:
-        print_exc()
+        node = provider.create_host(cpu, memory, name, group)
+    except Exception as e: # TODO What can get wrong here?
+        print_exc() # TODO Improve log
         return response_invalid_request(str(e))
 
     address = node.private_ips[0]
@@ -36,6 +37,82 @@ def create_host(provider_name, env):
     host.save()
 
     return response_created(address=address, id=host.id)
+
+
+@app.route("/<string:provider_name>/<string:env>/host/stop", methods=['POST'])
+def stop_host(provider_name, env):
+    data = request.get_json()
+    host_id = data.get("host_id", None)
+
+    # TODO improve validation and response
+    if not host_id:
+        return response_invalid_request("invalid data {}".format(data))
+
+    try:
+        host = Host.get(id=host_id)
+    except Host.DoesNotExist:
+        return response_not_found(host_id)
+
+    try:
+        provider_cls = get_provider_to(provider_name)
+        provider = provider_cls(env, host.engine)
+        provider.stop(host.identifier)
+    except Exception as e: # TODO What can get wrong here?
+        print_exc() # TODO Improve log
+        return response_invalid_request(str(e))
+
+    return response_ok()
+
+
+@app.route("/<string:provider_name>/<string:env>/host/start", methods=['POST'])
+def start_host(provider_name, env):
+    data = request.get_json()
+    host_id = data.get("host_id", None)
+
+    # TODO improve validation and response
+    if not host_id:
+        return response_invalid_request("invalid data {}".format(data))
+
+    try:
+        host = Host.get(id=host_id)
+    except Host.DoesNotExist:
+        return response_not_found(host_id)
+
+    try:
+        provider_cls = get_provider_to(provider_name)
+        provider = provider_cls(env, host.engine)
+        provider.start(host.identifier)
+    except Exception as e: # TODO What can get wrong here?
+        print_exc() # TODO Improve log
+        return response_invalid_request(str(e))
+
+    return response_ok()
+
+
+@app.route(
+    "/<string:provider_name>/<string:env>/host/<host_id>", methods=['DELETE']
+)
+def destroy_host(provider_name, env, host_id):
+    # TODO improve validation and response
+    if not host_id:
+        return response_invalid_request("invalid data")
+
+    try:
+        host = Host.get(id=host_id)
+    except Host.DoesNotExist:
+        return response_not_found(host_id)
+
+    try:
+        provider_cls = get_provider_to(provider_name)
+        provider = provider_cls(env, host.engine)
+        provider.destroy(host.group, host.identifier)
+    except Exception as e: # TODO What can get wrong here?
+        print_exc() # TODO Improve log
+        return response_invalid_request(str(e))
+
+    host.delete_instance()
+
+    return response_ok()
 
 
 @app.route(
@@ -56,12 +133,40 @@ def create_credential(provider_name, env):
         return response_created(success=success, id=str(credential_id))
 
 
+@app.route(
+    "/<string:provider_name>/<string:env>/credential/", methods=['DELETE']
+)
+def destroy_credential(provider_name, env):
+    try:
+        credential = CredentialAdd(provider_name, env, {})
+        deleted = credential.delete()
+    except Exception as e:
+        return response_invalid_request(str(e))
+
+    if deleted.deleted_count <= 0:
+        return response_not_found("{}-{}".format(provider_name, env))
+
+    return response_ok()
+
+
 def response_invalid_request(error, status_code=500):
     return _response(status_code, error=error)
 
 
+def response_not_found(identifier):
+    error = "Could not found with {}".format(identifier)
+    return _response(404, error=error)
+
+
 def response_created(status_code=201, **kwargs):
     return _response(status_code, **kwargs)
+
+
+def response_ok(message=None):
+    if not message:
+        message = "ok"
+
+    return _response(200, message=message)
 
 
 def _response(status, **kwargs):
