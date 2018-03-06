7f253c563987084800bd676935fee79c8e2209c4
otherpirate <mauro_murari@hotmail.com>
2018-03-06 16:42:28 -0300

    DB Port is integer

--- host_provider/settings.py
+++ host_provider/settings.py
@@ -5,14 +5,14 @@ LIBCLOUD_CA_CERTS_PATH = getenv("LIBCLOUD_CA_CERTS_PATH", None)
 
 
 MONGODB_HOST = getenv("MONGODB_HOST", "127.0.0.1")
-MONGODB_PORT = getenv("MONGODB_PORT", 27017)
+MONGODB_PORT = int(getenv("MONGODB_PORT", 27017))
 MONGODB_DB = getenv("MONGODB_DB", "host_provider")
 MONGODB_USER = getenv("MONGODB_USER", None)
 MONGODB_PWD = getenv("MONGODB_PWD", None)
 
 
 MYSQL_HOST = getenv("MYSQL_HOST", "127.0.0.1")
-MYSQL_PORT = getenv("MYSQL_PORT", 3306)
+MYSQL_PORT = int(getenv("MYSQL_PORT", 3306))
 MYSQL_DB = getenv("MYSQL_DB", "host_provider")
 MYSQL_USER = getenv("MYSQL_USER", "root")
 MYSQL_PWD = getenv("MYSQL_PWD", "")
