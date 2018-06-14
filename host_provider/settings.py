b5d17ec1af788c1bd5c44bb7e2d84916b64d31b2
Elesbom <rafael.goncalves@corp.globo.com>
2018-06-14 11:53:26 -0300

    Configre endpoint for mysql

--- host_provider/settings.py
+++ host_provider/settings.py
@@ -1,4 +1,5 @@
 from os import getenv
+import re
 
 
 LIBCLOUD_CA_CERTS_PATH = getenv("LIBCLOUD_CA_CERTS_PATH", None)
@@ -17,6 +18,15 @@ MYSQL_PORT = int(getenv("MYSQL_PORT", 3306))
 MYSQL_DB = getenv("MYSQL_DB", "host_provider")
 MYSQL_USER = getenv("MYSQL_USER", "root")
 MYSQL_PWD = getenv("MYSQL_PWD", "")
+DBAAS_MYSQL_ENDPOINT = getenv("BAAS_MYSQL_ENDPOINT")
+
+if DBAAS_MYSQL_ENDPOINT:
+    matched_string = re.search(
+        'mysql://(.*):(.*)@(.*):(\d+)/(.*)', DBAAS_MYSQL_ENDPOINT
+    )
+    if matched_string:
+        MYSQL_USER, MYSQL_PWD, MYSQL_HOST, MYSQL_PORT, MYSQL_DB  = matched_string.groups()
+
 
 MYSQL_PARAMS = {"database": MYSQL_DB, "host": MYSQL_HOST, "port": MYSQL_PORT}
 if MYSQL_USER:
