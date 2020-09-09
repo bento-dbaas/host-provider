d21630088ed5095cf5ca2aa890f2d5842d55434c
elesbom <rafael.goncalves@corp.globo.com>
2020-09-09 15:48:02 -0300

    Add K8s Provider on __init__

--- host_provider/providers/__init__.py
+++ host_provider/providers/__init__.py
@@ -1,7 +1,7 @@
 from host_provider.providers.base import ProviderBase
 from host_provider.providers.cloudstack import CloudStackProvider
 from host_provider.providers.aws import AWSProvider
-
+from host_provider.providers.k8s import K8sProvider
 
 def get_provider_to(provider_name):
     for cls in ProviderBase.__subclasses__():
