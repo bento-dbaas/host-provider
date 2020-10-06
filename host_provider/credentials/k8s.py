ad9e72bd4e40f014660c85cc299e27971d5f7829
Mauro Murari <mauroamurari@gmail.com>
2020-10-06 12:48:49 -0300

    Create image and file log config

--- host_provider/credentials/k8s.py
+++ host_provider/credentials/k8s.py
@@ -6,12 +6,32 @@ class CredentialK8s(CredentialBase):
     # This data should be in credential database
     CONFIGURATION_FILES = {
             "mongodb_4_2_3": "mongodb.conf"
-
+    }
+    LOG_FILES = {
+            "mongodb_4_2_3": "mongodb.log"
+    }
+    IMAGE_NAMES = {
+            "mongodb_4_2_3": "mongo"
+    }
+    IMAGE_VERSIONS = {
+            "mongodb_4_2_3": "4.2"
     }
 
     @property
     def configuration_file(self):
         return self.CONFIGURATION_FILES[self.engine]
+
+    @property
+    def log_file(self):
+        return self.LOG_FILES[self.engine]
+
+    @property
+    def image_name(self):
+        return self.IMAGE_NAMES[self.engine]
+
+    @property
+    def image_version(self):
+        return self.IMAGE_VERSIONS[self.engine]
 
     @property
     def _zones_field(self):
