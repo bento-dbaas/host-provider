819f0b3fa2f6a5538f6678abe79f2fdad8f7a34b
elesbom <rafael.goncalves@corp.globo.com>
2021-02-17 12:50:15 -0300

    Create method for create static ip

--- host_provider/providers/gce.py
+++ host_provider/providers/gce.py
@@ -5,13 +5,19 @@ from google.oauth2 import service_account
 
 from host_provider.credentials.gce import CredentialGce, CredentialAddGce
 from host_provider.providers.base import ProviderBase
-from host_provider.models import Host
+from host_provider.models import Host, IP
 
 
 LOG = logging.getLogger(__name__)
 
+
 class WrongStatusError(Exception):
     pass
+
+
+class StaticIPNotFoundError(Exception):
+    pass
+
 
 class GceProvider(ProviderBase):
 
@@ -30,6 +36,9 @@ class GceProvider(ProviderBase):
     @classmethod
     def get_provider(cls):
         return "gce"
+
+    def get_static_ip_by_name(self, name):
+        return IP.get(name=name)
 
     def build_credential(self):
         return CredentialGce(
@@ -77,8 +86,20 @@ class GceProvider(ProviderBase):
         source_disk_image = image_response['selfLink']
         offering = self.credential.offering_to(int(cpu), memory)
         zone = self.credential.zone
+        static_ip_id = kw.get('static_ip_id')
+        if not static_ip_id:
+            raise StaticIPNotFoundError(
+                'The id of static IP must be provided'
+            )
+        static_ip = self.get_static_ip_by_name(static_ip_id)
 
         machine_type = "zones/{}/machineTypes/{}".format(zone, offering)
+
+        network_interface = {
+            'subnetwork': self.credential.subnetwork,
+            'aliasIpRanges': [],
+            'networkIP': static_ip.address
+        }
 
         config = {
             'name': name,
@@ -97,18 +118,7 @@ class GceProvider(ProviderBase):
 
             # Specify a network interface with NAT to access the public
             # internet.
-            'networkInterfaces': [{
-                "subnetwork": self.credential.content['subnetwork'],
-                #"accessConfigs": [
-                #    {
-                #        "kind": "compute#accessConfig",
-                #        "name": "External NAT",
-                #        "type": "ONE_TO_ONE_NAT",
-                #        "networkTier": "STANDARD"
-                #    }
-                #],
-                "aliasIpRanges": []
-            }],
+            'networkInterfaces': [network_interface],
 
             # Allow the instance to access cloud storage and logging.
             'serviceAccounts': [{
@@ -121,7 +131,6 @@ class GceProvider(ProviderBase):
 
         }
 
-        #self.credential.zone
         instance = self.client.instances().insert(
             project=self.credential.project,
             zone=zone,
@@ -135,6 +144,37 @@ class GceProvider(ProviderBase):
             project=self.credential.project,
             zone=host.zone,
             instance=host.name
+        ).execute()
+
+    def create_static_ip(self, group, ip_name):
+        self.credential.before_create_host(group)
+        self.client.addresses().insert(
+            project=self.credential.project,
+            region=self.credential.region,
+            body={
+                'subnetwork': self.credential.subnetwork,
+                'addressType': 'INTERNAL',
+                'name': ip_name
+            }
+        ).execute()
+
+        self.wait_status_of_static_ip(ip_name, status="RESERVED")
+        ip_metadata = self.get_internal_static_ip(ip_name)
+
+        ip = IP(
+            name=ip_name,
+            group=group,
+            address=ip_metadata['address']
+        )
+        ip.save()
+
+        return ip
+
+    def destroy_static_ip(self, ip_name):
+        self.client.addresses().delete(
+            project=self.credential.project,
+            region=self.credential.region,
+            address=ip_name
         ).execute()
 
     def clean(self, name):
@@ -159,16 +199,20 @@ class GceProvider(ProviderBase):
     def _is_ready(self, host):
         pass
 
-    def create_host_object(self, provider, payload, env,
-                           created_host_metadata):
+    def associate_ip_with_host(self, host, static_ip_id):
+        ip = self.get_static_ip_by_name(static_ip_id)
+        ip.host = host
+        ip.save()
 
-        project = provider.credential.project
-        zone = provider.credential._zone
+    def create_host_object(self, provider, payload, env,
+                           created_host_metadata, static_ip_id):
+
+        zone = provider.credential.zone
         instance_name = payload['name']
 
-        self.wait_status(project, zone, instance_name, status='RUNNING')
+        self.wait_status_of_instance(instance_name, status='RUNNING')
 
-        instance = self.get_instance(project, zone, instance_name)
+        instance = self.get_instance(instance_name)
         address = instance['networkInterfaces'][0]['networkIP']
 
         host = Host(
@@ -181,26 +225,84 @@ class GceProvider(ProviderBase):
         host.save()
         return host
 
-    def get_instance(self, project, zone, instance_name):
-        return self.client.instances().get(
-            project=project,
-            zone=zone,
+    def get_instance(self, instance_name, execute_request=True):
+        request = self.client.instances().get(
+            project=self.credential.project,
+            zone=self.credential.zone,
             instance=instance_name
-        ).execute()
+        )
+        if execute_request:
+            return request.execute()
+        return request
 
-    def wait_status(self, project, zone, instance_name, status):
+    def get_internal_static_ip(self, ip_name, execute_request=True):
+        request = self.client.addresses().get(
+            project=self.credential.project,
+            region=self.credential.region,
+            address=ip_name
+        )
+        if execute_request:
+            return request.execute()
+        return request
+
+    def wait_status_of_static_ip(self, address_name, status):
+
+        request = self.get_internal_static_ip(
+            address_name,
+            execute_request=False
+        )
+
+        return self._wait_status_of(
+            request,
+            status,
+            required_fields=['address']
+        )
+
+    def wait_status_of_instance(self, instance_name, status):
+        request = self.get_instance(
+            instance_name,
+            execute_request=False
+        )
+
+        return self._wait_status_of(request, status)
+
+    def _wait_status_of(self, request, status, required_fields=None):
+        """
+        Wait resource get specific status through
+        status param.
+        u can pass `required_fields`
+        required_fields is a array with field name.
+        When u pass fields name, we will wait this field
+        exists in response resource.
+        Ex. required_fields=['age', 'name']
+        """
+
+        def resource_has_required_fields(resource, required_fields):
+            for field_name in required_fields:
+                if field_name not in resource:
+                    return False
+            return True
+
+        if required_fields is None:
+            required_fields = []
         attempts = 1
         while attempts <= 60:
 
-            instance = self.get_instance(project, zone, instance_name)
-            vm_status = instance['status']
-            if vm_status == status:
+            resource = request.execute()
+            resource_status = resource['status']
+            if (resource_status == status and
+                    resource_has_required_fields(resource, required_fields)):
                 return True
 
             attempts += 1
             sleep(5)
 
-        raise WrongStatusError(
-            "It was expected status: {} got: {}".format(status, vm_status)
+        err_msg = "It was expected status: {} got: {}.".format(
+            status, resource_status
         )
-
+        if required_fields:
+            err_msg += (" And It expected required fields: {} "
+                        "the resource has this fields: {}").format(
+                            required_fields, resource.keys()
+                        )
+        raise WrongStatusError(err_msg)
